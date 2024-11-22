c72fce0b2107ccf6aad68d1a5f442a667171086a
Yunki0 <desirelionel109@gmail.com>
2024-11-22 13:08:35 +0000

    Ajout du paramètre permettant de rediriger en cliquant sur les balises asides

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 window.onload = function() {
      const loader = document.getElementById('loader');
-//     loader.style.display = 'none';
     
     // Ajoute un délai de 2 secondes avant de masquer l'overlay de chargement
      setTimeout(function() {
          loader.style.display = 'none';
-     }, 800); // 800 millisecondes = 0.8 secondes
+     },800); // 800 millisecondes = 0.8 secondes
 };
 
+// script pour redirection
+document.querySelector('.container').addEventListener('click', function() {
+    const url = this.getAttribute('data-url');
+    window.location.href = url; // Redirige vers l'URL
+});
